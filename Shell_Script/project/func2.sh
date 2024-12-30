27f77664615a3c0d979e821b5f4073157b11ba7f
Vibish <vibishdevops@gmail.com>
2024-12-31 01:04:46 +0530

    Changes Func

---
+++ Shell_Script/project/func2.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+source ./function.sh
+callback
+
+echo "Function called From Another Scirpt...!!"
---
+++ Shell_Script/project/function.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+callback() {
+    local loc_var="I am in local"
+    echo loc_var
+}
+
